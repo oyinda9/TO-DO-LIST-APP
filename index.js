d01f6c3b3613354327eca5408f51544643cf36a6
oluwatobimaxwell <tobisholanke@gmail.com>
2024-02-17 21:39:49 +0200

    added app files

---
+++ index.js
@@ -0,0 +1,49 @@
+const taskList = [];
+
+const addButtonElement = document.getElementById("add-button");
+const taskListElement = document.getElementById("tast-list");
+
+addButtonElement.addEventListener("click", function () {
+  const taskName = prompt("Enter your task: ");
+  if (taskName) {
+    const taskObject = {
+      name: taskName,
+      time: new Date(),
+      status: false,
+    };
+    taskList.push(taskObject);
+    updateUI();
+  }
+});
+
+function formatTime(date) {
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  const ampm = hours >= 12 ? "pm" : "am";
+
+  hours = hours % 12;
+  hours = hours ? hours : 12; // the hour '0' should be '12'
+  minutes = minutes < 10 ? "0" + minutes : minutes;
+
+  const strTime = hours + ":" + minutes + ampm;
+  return strTime;
+}
+
+function updateUI() {
+  taskListElement.innerHTML = "";
+  let taskUis = "";
+  for (let i = 0; i < taskList.length; i++) {
+    const task = taskList[i];
+
+    taskUis += `<div class="list-container">
+                    <input type="checkbox" id="veg" name="veg" value="veg" ${task.status ? "checked" : ""}>
+                    <label  ${
+                      task.status
+                        ? `style="text-decoration: line-through; color: white;"`
+                        : ""
+                    }>${task.name}</label>
+                    <span>${formatTime(task.time)}</span>
+                </div>`;
+  }
+  taskListElement.innerHTML = taskUis;
+}
