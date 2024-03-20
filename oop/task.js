404a34dd3b481ec4546612af9e6d407e5ee6bb06
sholanke precious <sholankeprecious3@gmail.com>
2024-03-20 14:16:49 +0100

    added a property to check if a task already exists

--- oop/task.js
+++ oop/task.js
@@ -10,10 +10,19 @@ class TaskList {
     tasks = [];
 
     // TODO: modify addTask to update task with existing name
-    addTask(task){
-        this.tasks.push(task);
-    }
 
+    taskExists(taskName) {
+        return this.tasks.some(task => task.name === taskName);
+    }
+    addTask(task) {
+        if (!this.taskExists(task.name)) {
+            this.tasks.push(task);
+            console.log("Task added successfully.");
+        } else {
+            console.log("Task with the same name already exists.");
+        }
+            // this.tasks.push(task);
+    }
     removeTask(index){
         this.tasks.splice(index, 1);
     }
@@ -33,10 +42,12 @@ const tasks = new TaskList();
 const task1 = new Task("Task 1", false, new Date());
 const task2 = new Task("Task 2", true, new Date());
 const task3 = new Task("Task 3", true, new Date());
+const task4 = new Task("Task 3", true, new Date());
 
 tasks.addTask(task1)
 tasks.addTask(task3)
 tasks.addTask(task2)
+tasks.addTask(task4)
 
 console.log(tasks);
 
